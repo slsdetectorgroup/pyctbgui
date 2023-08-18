e855074a93f5abe5c1b215b768154961d44914a3
Bechir <bechir.braham@psi.ch>
2023-08-18 15:35:31 +0200

    debuggin gh actions: restore test alias file

--- tests/test_alias_file.py
+++ tests/test_alias_file.py
@@ -4,34 +4,35 @@ from pyctbgui import alias_utility as at
 
 
 def test_read_non_existing_file_throws():
-    with pytest.raises(FileNotFoundError):
+    with pytest.raises(Exception):
         at.read_alias_file('saijvcaiewjrvijaerijvaeoirvjveiojroiajgv')
 
 
 def test_parse_sample_file():
     fpath = Path(__file__).parent / 'data/moench04_start_trigger.alias'
-    bit_names, bit_plots, bit_colors, adc_names, adc_plots, adc_colors, dac_names,\
-    sense_names, power_names, pat_file_name = at.read_alias_file(fpath)
+    bit_names, bit_plots, bit_colors, adc_names, adc_plots, adc_colors, dac_names, sense_names, power_names, pat_file_name = at.read_alias_file(
+        fpath)
 
     assert len(bit_names) == 64
 
     true_names = [
         'gHG', 'DGS6', 'pulse', 'DGS7', 'clearSSR', 'DGS8', 'inSSR', 'DGS9', 'clkSSR', 'clk_fb', 'prechargeConnect',
         'clk', 'clear', 'shr_out_15', 'bypassCDS', 'Sto2', 'ENprechPre', 'Sto0_bot', 'pulseOFF', 'Sto1', 'BYPASS',
-        'connCDS', 'Dsg3', 'OutSSRbot', 'resCDS', 'res', 'shr_out_31', 'Dsg1', 'READ', 'Sto0_top', 'resDGS', 'shr_in',
-        None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 'DGS23',
-        'DGS31', 'DGS22', 'DGS30', 'DGS21', 'DGS29', 'DGS20', 'DGS28', 'DGS19', 'DGS27', 'DGS18', 'DGS26', 'DGS17',
-        'DGS25', 'DGS16', 'DGS24'
+        'connCDS',
+        'Dsg3', 'OutSSRbot', 'resCDS', 'res', 'shr_out_31', 'Dsg1', 'READ', 'Sto0_top', 'resDGS', 'shr_in',
+        None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+        'DGS23', 'DGS31',
+        'DGS22', 'DGS30', 'DGS21', 'DGS29', 'DGS20', 'DGS28', 'DGS19', 'DGS27', 'DGS18', 'DGS26', 'DGS17', 'DGS25',
+        'DGS16',
+        'DGS24'
     ]
 
-    true_plot = [
-        None, True, None, True, None, True, None, True, None, None, None, None, None, None, None, None, None, None,
-        None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-        None, None, None, None, None, None, None, None, None, None, None, None, True, True, True, True, True, True,
-        True, True, True, True, True, True, True, True, True, True
-    ]
+    true_plot = [None, True, None, True, None, True, None, True, None, None, None, None, None, None, None, None, None,
+                 None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+                 None, None, None, None, None, None, None, None, None, None, None, None, None, None, True, True, True,
+                 True, True, True, True, True, True, True, True, True, True, True, True, True]
 
-    #Make sure we didn't mess up anything in this file
+    # Make sure we didn't mess up anything in this file
     assert len(true_names) == len(bit_names)
     for value, reference in zip(bit_names, true_names):
         assert value == reference
@@ -40,12 +41,12 @@ def test_parse_sample_file():
     for value, reference in zip(bit_plots, true_plot):
         assert value == reference
 
-    #sense names
+    # sense names
     true_sese = ['Vcc_iochip', 'Va+', 'Vcc_int', 'T_boa.(C)', 'Vsh', 'Vcc1.8D', 'Vdd_ps', 'Vcc1.8A']
     assert len(true_sese) == 8
     assert true_sese == sense_names
 
-    #ADC
+    # ADC
 
     true_adc = [
         'SCOL9',
